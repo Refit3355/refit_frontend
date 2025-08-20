8fe472e4299a1032c0d18d266b0b05ddf63dbec7
price126 <hwjun12@gmail.com>
2025-08-21 07:43:15 +0900

    [FEAT] 더미데이터 추가

--- app/src/main/java/com/refit/app/ui/fake/DemoProducts.kt
+++ app/src/main/java/com/refit/app/ui/fake/DemoProducts.kt
@@ -1,2 +1,17 @@
 package com.refit.app.ui.fake
 
+import com.refit.app.data.product.model.Product
+
+object DemoProducts {
+    operator fun invoke(n: Int): List<Product> = List(n) { i ->
+        Product(
+            id = (i + 1).toLong(),
+            brand = "라블",
+            name = "다이브인 저분자 히알루론산",
+            image = "",
+            price = 36500,
+            discountedPrice = 19500,
+            discountRate = 47
+        )
+    }
+}
