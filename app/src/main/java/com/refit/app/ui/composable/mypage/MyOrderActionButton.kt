809ae50a28498d5e53a46e6ff71bef2d952a0632
price126 <hwjun12@gmail.com>
2025-08-24 02:48:02 +0900

    [FEAT] 마이페이지 버튼 Composable 추가

--- app/src/main/java/com/refit/app/ui/composable/mypage/MyOrderActionButton.kt
+++ app/src/main/java/com/refit/app/ui/composable/mypage/MyOrderActionButton.kt
@@ -1,2 +1,45 @@
 package com.refit.app.ui.composable.mypage
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.refit.app.ui.theme.Pretendard
+
+@Composable
+fun MyOrderActionButton(
+    text: String,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = {}
+) {
+    Box(
+        modifier = modifier
+            .height(30.dp)
+            .background(Color.White, shape = RoundedCornerShape(8.dp))
+            .border(
+                width = 1.dp,
+                color = Color(0xFFCCCCCC),
+                shape = RoundedCornerShape(8.dp)
+            )
+            .clickable { onClick() },
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = text,
+            fontFamily = Pretendard,
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Medium,
+            color = Color(0xFF999999)
+        )
+    }
+}
